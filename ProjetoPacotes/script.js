8a02303d70b061fc0de5812b30a3a2538711c4b0
LucasNotaro <lucas.sn@live.com>
2023-10-24 22:26:30 -0300

    pacotes

--- ProjetoPacotes/script.js
+++ ProjetoPacotes/script.js
@@ -1,14 +1,51 @@
-function calcularDesconto()
+let valorPacote;
+let totalAdicionais;
+ 
+function calcularTotalViagem(){
+    calcularPacote();
+    calcularAdicionais();
+    let totalFinal = totalAdicionais + valorPacote;
+    nome= document.getElementById('nome').value
+    document.getElementById("mensagem").innerHTML = `${nome} irá pagar ${valorPacote} pelo pacote e ${totalAdicionais} pelos adicionais `;
+ 
+}
+ 
+function calcularAdicionais(){
+    totalAdicionais =0;
+ 
+    let adicionais= document.querySelectorAll("input[name='adicionais']");
+    console.log(adicionais);
+    for (let i=0; i< adicionais.length; i= i+1)
+    {
+        if (adicionais[i].checked == true)
+        {
+            totalAdicionais = totalAdicionais + parseFloat(adicionais[i].value);
+        }
+    }
+    alert(`Adicionais: ${totalAdicionais}`);
+}
+ 
+function calcularPacote()
 {
     let qtdPacotes = document.querySelectorAll("input[name='pacotes']").length;
-    console.log(qtdPacotes);
+    //console.log(qtdPacotes);
  
     let pacotes = document.querySelectorAll("input[name='pacotes']");  
-    console.log(pacotes);
-
-//                i < pacotes.lenght
-    for (var i=0; i< qtdPacotes; i++)
+    //console.log(pacotes);
+ 
+  //let valor =  document.querySelector("input[name='pacotes']:checked").value;
+    //            i < pacotes.length
+    for (var i=0; i < qtdPacotes; i++)
     {
-        console.log(pacotes[i].value);
+        if (   pacotes[i].checked == true    )
+        {
+          //  console.log(pacotes[i].value);
+            valorPacote = Number(pacotes[i].value);
+            break;
+        }
     }
 }
+
+// let valor = document.querySelector("input[name='pacotes']:checked").value;
+
+
