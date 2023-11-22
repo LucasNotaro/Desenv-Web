7fcc3ffb91024b319bbf0213b3c1e71ce23ea801
LucasNotaro <lucas.sn@live.com>
2023-11-21 22:14:56 -0300

    atl

--- ProjetoAPI/script.js
+++ ProjetoAPI/script.js
@@ -1,6 +1,26 @@
 
 elemento = document.getElementById("pesquisa");
 
+
+let key = "9c6b7b9bc735f772b35e967ac095535c";
+let bandeira = "https://flagsapi.com";
+let imagem = "https://source.unsplash.com/1600x900/?city"
+
+
+ x= document.getElementById("pesquisaClima")
+ x.addEventListener('click', async function()
+ {
+     cidade = document.getElementById("cidade").value;
+     const apiClimaURL = `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&units=metric&appid=${key}&lang=pt_br`;
+     let respostaClima = await fetch(apiClimaURL)
+     let objdados = await respostaClima.json()
+     let temperatura = objdados.main.temp;
+     let descricao = objdados.weather[0].description
+     document.getElementById('resultado').innerText = 
+                       `Hoje está ${temperatura} e ${descricao}`
+ })
+
+
 elemento.addEventListener('click', async function(){
     document.getElementById('resultado').innerText = ""
         var valor= document.getElementById("cep").value
@@ -20,7 +40,7 @@ elemento.addEventListener('click', async function(){
                document.getElementById('resultado').innerText = "CEP Não Existe";
             else
                document.getElementById('resultado').innerText = dados.logradouro + " "+ dados.bairro
-    
+                document.getElementById('cidade').value = dados.localidade
     
      }
           else
